61cb3757cbadf1fc36bd082dbc8a2365a9a5514d
mainlyetcetera <ericcampbell294@gmail.com>
2022-01-06 17:13:25 -0600

    chore: add first test dir

---
+++ client/src/components/__tests__/Board.test.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Board from './components/Board';
+
+describe('can use describe (duh...)', () => {
+  test.todo('tells me something about the board')
+})

667fe442f387217f841a216e6f197b458faf858a
mainlyetcetera <ericcampbell294@gmail.com>
2022-01-08 19:45:47 -0600

    chore: mv Board, test exist

--- client/src/components/__tests__/Board.test.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Board from './components/Board';
-
-describe('can use describe (duh...)', () => {
-  test.todo('tells me something about the board')
-})

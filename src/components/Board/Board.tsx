618f8b1516c5c003811cd5a35d8698c7310d5077
Eric Campbell <eric.campbell@intevity.com>
2023-02-15 18:19:58 -0500

    add board

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,11 @@
+import { Chessboard } from 'react-chessboard';
+
+const Board = () => {
+    return (
+        <>
+            <Chessboard id="board" />
+        </>
+    );
+}
+
+export { Board };

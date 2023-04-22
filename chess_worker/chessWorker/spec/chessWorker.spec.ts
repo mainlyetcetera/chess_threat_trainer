49d0e3f269939786ef72ee06d68bd8bdae45ffb0
katana_fleet <ericcampbell294@gmail.com>
2023-04-22 14:06:47 -0400

    add jest to worker

---
+++ chess_worker/chessWorker/spec/chessWorker.spec.ts
@@ -0,0 +1,15 @@
+describe('the chess worker', () => {
+  let w: null;
+
+  beforeAll(() => {
+    w = null;
+  });
+
+    it('can run a test', () => {
+        expect(true).toBe(true);
+    });
+
+    it('exists', () => {
+        expect(w).toBeTruthy();
+    });
+});
